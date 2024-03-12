b42bef01e66031fa17449b14df88794c2e4b842f
Ngo Minh Phuong <minhphuong49th@gmail.com>
2024-03-12 13:46:54 +0700

    Update code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,9 +3,9 @@ resource "aws_vpc" "vpc" {
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  enable_classiclink   = false
-  enable_classiclink_dns_support = false
-  assign_generated_ipv6_cidr_block = false
+  #enable_classiclink   = false
+  #enable_classiclink_dns_support = false
+  #assign_generated_ipv6_cidr_block = false
 
 
 
